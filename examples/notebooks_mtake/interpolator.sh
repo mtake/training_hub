b0a97cdbb56f0f2354fe4caf9516becfa8bbdfae
Mikio Takeuchi <mtake@jp.ibm.com>
2025-10-07 03:46:23 -0400

    update script

--- examples/notebooks_mtake/interpolator.sh
+++ examples/notebooks_mtake/interpolator.sh
@@ -17,7 +17,8 @@ echo "XXX LOGFILE ${LOGFILE}" | tee -a ${LOGFILE}
 echo "XXX DATETIME ${START_TIME_STR}" | tee -a ${LOGFILE}
 
 ORIG="ibm-granite/granite-3.3-8b-instruct"
-TRAINED="experiments/sft_comprehensive_example_granite-3.3-8b-instruct_teigaku-genzei-ibm-v6_20251002_015942/hf_format/samples_50253"
+#TRAINED="experiments/sft_comprehensive_example_granite-3.3-8b-instruct_teigaku-genzei-ibm-v6_20251002_015942/hf_format/samples_50253"
+TRAINED="experiments/osft_comprehensive_example_granite-3.3-8b-instruct_teigaku-genzei-ibm-v6_20251002_015942/hf_format/samples_50253.0"
 INTERPOLATED="${TRAINED}_interp"
 
 MODEL_PATH="${ORIG}"
