55d8f2e9f4cd8a38b0d77f86dfa1d87970b5095b
Mikio Takeuchi <mtake@jp.ibm.com>
2025-10-15 09:36:24 -0400

    WIP granite 4

--- examples/scripts_mtake/osft_granite4_example_mtake.py
+++ examples/scripts_mtake/osft_granite4_example_mtake.py
@@ -35,8 +35,15 @@ from training_hub import osft
 
 # Derived from generic_7b_example in examples/notebooks/osft_comprehensive_tutorial.ipynb
 granite4_example = {
-    "model_name": "Granite-4.0-H-Small",
+    # @@@ahoaho XXX
+    "model_name": "Granite-4.0-H-Small",  # 4GPU ERR Connection closed by localRank N
     "model_path": "ibm-granite/granite-4.0-h-small",  # HuggingFace model name or local path
+    # "model_name": "Granite-4.0-H-Tiny",  # 2GPU OK
+    # "model_path": "ibm-granite/granite-4.0-h-tiny",  # HuggingFace model name or local path
+    # "model_name": "Granite-4.0-H-Micro",  # 2GPU OK
+    # "model_path": "ibm-granite/granite-4.0-h-micro",  # HuggingFace model name or local path
+    # "model_name": "Granite-4.0-Micro",  # 2GPU OK
+    # "model_path": "ibm-granite/granite-4.0-micro",  # HuggingFace model name or local path
     "example_unfreeze_rank_ratio": 0.3,  # Balanced preservation vs adaptation
     "example_max_tokens_per_gpu": 10000,
     "example_max_seq_len": 4096,
