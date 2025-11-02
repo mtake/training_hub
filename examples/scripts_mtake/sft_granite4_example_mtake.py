bb63c046036bbcce3a2d87b40457ea7dfcde3404
Mikio Takeuchi <mtake@jp.ibm.com>
2025-11-03 02:05:51 +0900

    WIP granite4hs

--- examples/scripts_mtake/sft_granite4_example_mtake.py
+++ examples/scripts_mtake/sft_granite4_example_mtake.py
@@ -44,7 +44,7 @@ granite4hs_example = {
     "model_path": "ibm-granite/granite-4.0-h-small",  # HuggingFace model name or local path
     "example_min_nproc_per_node": 8,
     # @@@ahoaho XXX
-    # Suggested by Mustafa
+    # OK with 8xA100, 1200MB
     "example_max_tokens_per_gpu": 1000,
     "example_max_seq_len": 150,
     "example_batch_size": 128,
@@ -52,6 +52,14 @@ granite4hs_example = {
     "kwargs": {
         "fsdp_options": FSDPOptions(cpu_offload_params=True),
     },
+    # # TODO with 8xA100, 1600MB
+    # "example_max_tokens_per_gpu": 25000,
+    # "example_max_seq_len": 20000,
+    # "example_batch_size": 256,
+    # "example_learning_rate": 2e-5,
+    # "kwargs": {
+    #     "fsdp_options": FSDPOptions(cpu_offload_params=True),
+    # },
 }
 granite4ht_example = {
     **granite4_example_template,
