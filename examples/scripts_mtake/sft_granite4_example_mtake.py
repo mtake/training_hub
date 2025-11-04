6e49dbbb13971be3fc05db86def9c7046ba1540d
Mikio Takeuchi <mtake@jp.ibm.com>
2025-11-04 17:08:27 +0900

    WIP granite4hs

--- examples/scripts_mtake/sft_granite4_example_mtake.py
+++ examples/scripts_mtake/sft_granite4_example_mtake.py
@@ -52,8 +52,16 @@ granite4hs_example = {
     #     "fsdp_options": FSDPOptions(cpu_offload_params=True),
     # },
     # OK with 8x A100 gpus and 1600 MB memory, Finished in 16.6 hours
+    # "example_max_tokens_per_gpu": 25000,
+    # "example_max_seq_len": 8192,
+    # "example_batch_size": 128,
+    # "example_learning_rate": 2e-5,
+    # "kwargs": {
+    #     "fsdp_options": FSDPOptions(cpu_offload_params=True),
+    # },
+    # WIP with 8x A100 gpus and 1800 MB memory
     "example_max_tokens_per_gpu": 25000,
-    "example_max_seq_len": 8192,
+    "example_max_seq_len": 20000,
     "example_batch_size": 128,
     "example_learning_rate": 2e-5,
     "kwargs": {
